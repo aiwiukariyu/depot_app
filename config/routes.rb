dabe4e06ed0a02ba83d26348b33c817d9f045c86
aiwiukariyu <sky.red.storm@gmail.com>
2020-08-13 16:58:05 +0700

    respond email with email

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
-  
+  resources :support_request, only: [:index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
